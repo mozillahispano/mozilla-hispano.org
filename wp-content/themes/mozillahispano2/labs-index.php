238f7f57bce94e7d49b9ec55f9c9b6ed62fb98b1
Fernando Agüero <fjaguero@gmail.com>
2012-11-28 01:37:26 +0100

    Fix Github widget load URLs

--- wp-content/themes/mozillahispano2/labs-index.php
+++ wp-content/themes/mozillahispano2/labs-index.php
@@ -6,8 +6,9 @@ get_header(); ?>
 
 <!-- Carga del Javascript y estilos del bloque de Github -->
 
-	<link rel="stylesheet" href="css/github.min.css">
-	<script src="js/jquery.github.min.js"></script>
+	<link rel="stylesheet" href="<?php echo get_bloginfo ('template_url');?>/css/github.min.css">
+	<script src="<?php echo get_bloginfo ('template_url');?>/js/jquery.github.min.js"></script>
+
 	<script type="text/javascript">
 		$(document).ready(function(){
 			$("#github-block").github();
