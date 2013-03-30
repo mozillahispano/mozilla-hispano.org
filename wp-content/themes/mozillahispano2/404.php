479069d3b6500cf8f1b58cf37ca949937723cca5
Jorge Villalobos <jorge@xulforge.com>
2013-03-30 12:36:25 -0500

    Crear una página 404 bonita, Issue #24
    La versión sencilla, copiada del 404 de MDN. Tal vez requiera ajustes
    para alinear bien los ojos.

--- wp-content/themes/mozillahispano2/404.php
+++ wp-content/themes/mozillahispano2/404.php
@@ -7,17 +7,19 @@
 get_header();
 ?>
 
-	<div id="contenido">
-		<div class="portada-individual">
-			<div class="texto-portada-individual">
-				<h2 class="post-title">Página no encontrada</h2>
-				
-				<p>Esta página no existe. Por favor, use el buscador para encontrar lo que estaba buscando</p>
-				
-			</div>
-		</div>
-		
-		<?php get_sidebar(); ?>
-	</div>
+  <div id="contenido">
+    <div class="portada-individual">
+      <div class="texto-portada-individual">
+        <h2 class="post-title">Página no encontrada</h2>
+        <div id="beastainer">
+          <img id="beast404le" src="img/beast-404_LE.png" alt="">
+          <img id="beast404re" src="img/beast-404_RE.png" alt="">
+          <img class="beast 404" src="img/beast-404.png" alt="">
+        </div>
+      </div>
+    </div>
+
+    <?php get_sidebar(); ?>
+  </div>
 
 <?php get_footer(); ?>
