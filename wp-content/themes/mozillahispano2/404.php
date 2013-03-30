4fc86acf4f4f5f2206af1a77f19a765b0c7a2fb6
Jorge Villalobos <jorge@xulforge.com>
2013-03-30 13:39:05 -0500

    Más información para la página 404

--- wp-content/themes/mozillahispano2/404.php
+++ wp-content/themes/mozillahispano2/404.php
@@ -16,6 +16,14 @@ get_header();
           <img id="beast404re" src="/wp-content/themes/mozillahispano2/img/beast-404_RE.png" alt="" />
           <img class="beast 404" src="/wp-content/themes/mozillahispano2/img/beast-404.png" alt="Página no encontrada" />
         </div>
+        <div id="error404-info">
+          <p>&Eacute;sta p&aacute;gina no fue encontrada, pero aqu&iacute; tienes varias opciones:</p>
+          <ul>
+            <li>Si necesitas ayuda, te podemos ayudar en el <a href="/foro/">foro de asistencia</a>.</li>
+            <li>Si quieres colaborar con nosotros, visita la <a href="/documentacion/Colabora">p&aacute;gina de colaboraci&oacute;n</a>.</li>
+            <li>Para iniciar de nuevo, ve a nuestra <a href="/">p&aacute;gina de inicio</a>.</li>
+          </ul>
+        </div>
       </div>
     </div>
 
