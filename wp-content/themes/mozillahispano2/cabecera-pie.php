a9cfc2983466f888164ffd855fe90c53ba9f3380
Fernando García <fernando.garciagomez.01@telefonica.com>
2020-07-22 15:29:10 +0200

    Propuesta actualización de enlaces cabecera

--- wp-content/themes/mozillahispano2/cabecera-pie.php
+++ wp-content/themes/mozillahispano2/cabecera-pie.php
@@ -14,64 +14,28 @@ $temp= <<<PINTA
 							<ul class="submenu">
 								<li><a href="https://support.mozilla.org/es/" title="Documentos, artículos y preguntas frecuentes de ayuda a los usuarios">Documentación</a></li>
 								<li><a href="https://support.mozilla.org/es/questions/new" title="Haz una pregunta en el foro de asistencia">Haz una pregunta</a></li>
-								<li><a href="/documentacion/Asistencia" title="Proyectos">Proyectos</a></li>
 							</ul>
 						</li>
 						<li><span title="Ayuda a difundir Mozilla en español">Difusión</span>
 							<ul class="submenu">
 								<li><a href="/difusion/">Promociona Mozilla</a></li>
-								<!-- <li><a href="https://foro.mozilla-hispano.org/c/difusion-eventos" title="Foro de difusión">Foros</a></li> -->
-								<li><a href="/documentacion/Eventos" title="Eventos Mozilla">Eventos</a></li>
-								<li><a href="/documentacion/Difusi%C3%B3n" title="Proyectos de difusión">Proyectos</a></li>
+								<li><a href="https://community.mozilla.org/groups/mozilla-hispano/?view=events" title="Eventos Mozilla">Eventos</a></li>
 							</ul>
 						</li>
 						<li><a href="https://discourse.mozilla.org/c/community-portal/mozilla-hispano" title="Foros de discusión">Foros</a></li>
-						<li><span title="Desarrolla con Labs">Labs</span>
-							<ul class="submenu">
-								<li><a href="/labs/" title="Centro de desarrollo e innovación en la plataforma Mozilla">Blog y proyectos</a></li>
-								<!-- <li><a href="https://foro.mozilla-hispano.org/c/labs">Foros</a></li> -->
-							</ul>
-						</li>
-						<li><span>Comunidad</span>
-							<ul class="submenu">
-								<li><a href="/planet/" title="Artículos de opinión, mensajes y fotos de los miembros de la comunidad">Planet</a></li>
-								<li><a href="/documentacion/Colaboradores" title="Listado de colaboradores">Colaboradores</a></li>
-								<li><a href="/ar/">Argentina</a></li>
-								<li><a href="/bo/">Bolivia</a></li>
-								<li><a href="/co/">Colombia</a></li>
-								<li><a href="/cr/">Costa Rica</a></li>
-								<li><a href="/cl/">Chile</a></li>
-								<li><a href="/cu/">Cuba</a></li>
-								<li><a href="/es/">España</a></li>
-								<li><a href="/ec/">Ecuador</a></li>
-								<li><a href="/ni/">Nicaragua</a></li>
-								<li><a href="/mx/">México</a></li>
-								<li><a href="/py/">Paraguay</a></li>
-								<li><a href="/pe/">Perú</a></li>
-								<li><a href="/uy/">Uruguay</a></li>
-								<li><a href="/ve/">Venezuela</a></li>
-							</ul>
-						</li>
-						<li><span title="Únete y colabora con Mozilla Hispano">Participa</span>
-							<ul class="submenu">
-								<li><a href="/documentacion/Colabora">Cómo participar</a></li>
-								<li><a href="/documentacion/Recursos_para_colaboradores" title="Recursos para colaboradores">Recursos para colaboradores</a></li>
-								<li><a href="https://discourse.mozilla.org/c/community-portal/mozilla-hispano">Foro de discusión</a></li>
-								<li><a href="/documentacion/Proyectos">Proyectos</a></li>
-								<li><a href="/documentacion/Tareas">Tareas</a></li>
-								<li><a href="/documentacion/Reuniones">Reuniones</a></li>
-							</ul>
-						</li>
+						<li><a href="https://community.mozilla.org/groups/mozilla-hispano/">Comunidad</a></li>
+						<li><a href="https://discourse.mozilla.org/t/leeme-como-participar-en-la-comunidad/64161" title="Únete y colabora con Mozilla Hispano">Participa</a></li>
 					</ul>
 				</div>
 				<div id="social">
 					<ul>
 						<li><a title="Síguenos en Twitter" id="twitter-icon" href="http://twitter.com/mozilla_hispano">Síguenos en Twitter</a></li>
 						<li><a title="Síguenos en Facebook" id="facebook-icon" href="http://www.facebook.com/mozillahispano">Síguenos en Facebook</a></li>
-						<li><a title="Síguenos en Google Plus" id="gplus-icon" href="https://plus.google.com/113725577998863887008/posts">Síguenos en Google Plus</a></li>
 						<li><a title="Nuestros vídeos en YouTube" id="youtube-icon" href="http://www.youtube.com/mozillahispano">Nuestros vídeos en YouTube</a></li>
-						<li><a title="Nuestros fotos en Flickr" id="flickr-icon" href="http://www.flickr.com/photos/tags/mozillahispano/">Nuestras fotos en Flickr</a></li>
-						<li><a title="Feed RSS" id="rss-icon" href="http://feeds.mozilla-hispano.org/mozillahispano">Feed RSS</a></li>
+						<li><a title="Nuestro foro" id="discourse-icon" href="https://discourse.mozilla.org/c/community-portal/mozilla-hispano/304">Nuestro foro</a></li>
+						<li><a title="Nuestro portal comunitario" id="community-icon" href="https://community.mozilla.org/groups/mozilla-hispano/">Nuestro portal comunitario</a></li>
+						<li><a title="Nuestra herramienta de chat" id="matrix-icon" href="https://chat.mozilla.org/#/room/#mozilla-hispano:mozilla.org">Nuestra herramienta de chat</a></li>
+						<li><a title="Feed RSS" id="rss-icon" href="https://www.mozilla-hispano.org/feed/">Feed RSS</a></li>
 					</ul>
 				</div>
 </div>
@@ -195,10 +159,7 @@ function pintaCss()
 $temp= <<<PINTA
 	<link type="image/x-icon" href="/favicon.ico" rel="shortcut icon" />
 
-	<link rel="alternate" type="application/rss+xml" title="Noticias de Mozilla Hispano" href="http://feeds.mozilla-hispano.org/mozillahispano" />
-	<link rel="alternate" type="application/rss+xml" title="Artículos en Planet Mozilla Hispano" href="http://feeds.mozilla-hispano.org/mozillahispano-planet" />
-	<link rel="alternate" type="application/rss+xml" title="El Podcast de Mozilla Hispano" href="http://feeds.mozilla-hispano.org/mozillahispano-podcast" />
-
+	<link rel="alternate" type="application/rss+xml" title="Noticias de Mozilla Hispano" href="https://www.mozilla-hispano.org/feed/" />
 	<link rel="search" type="application/opensearchdescription+xml" title="Mozilla Hispano - Noticias" href="/archivos/noticias.xml" />
 	<link rel="search" type="application/opensearchdescription+xml" title="Mozilla Hispano - Documentación" href="/archivos/documentacion.xml" />
 
